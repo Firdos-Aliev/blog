8eccddeced8f8bd34697c154b426829dc7cf9714
Firdos-Aliev <firdos1234567891@gmail.com>
2021-04-27 19:42:29 +0300

    doc_strings

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,16 +3,33 @@ from django.contrib.auth import get_user_model
 
 
 class Post(models.Model):
+    """
+    Stores a posts
+    Related to:
+        :model: `User`
+    """
     name = models.CharField(verbose_name='Название', max_length=256)
     text = models.TextField(verbose_name='Текст')
     time = models.TimeField(verbose_name='Время добавления/обновления', auto_now=True, auto_now_add=False)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='Пользователь')
     is_active = models.BooleanField(verbose_name='Активный', default=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Comments(models.Model):
+    """
+    Stores a comments for posts
+    Related to:
+        :model: `User`
+        :model: `Post`
+    """
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='Пользователь')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост')
     text = models.TextField(verbose_name='Текст')
     time = models.TimeField(verbose_name='Время добавления/обновления', auto_now=True, auto_now_add=False)
     is_active = models.BooleanField(verbose_name='Активный', default=True)
+
+    def __str__(self):
+        return self.text
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,4 +4,15 @@ from mainapp.models import Post
 
 
 class PostList(ListView):
+    """
+    
+    Display all post from model.Post and their cooments
+
+    **Context**
+    :model: `mainapp.models.Post`
+
+    **Template**
+    :template: `mainapp/post_list.html`
+    
+    """
     model = Post
