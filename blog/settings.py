ba748ecd463185c2c55d5c54066fdfe40d33d782
Firdos-Aliev <firdos1234567891@gmail.com>
2021-05-20 17:35:10 +0300

    postgresql

--- blog/settings.py
+++ blog/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+        'NAME': 'blog',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'blog_admin',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        }
+    }
 
 
 # Password validation
